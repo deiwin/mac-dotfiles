714ba948292ade1f52e0109250f0a6f6738869ae
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-10-26 12:55:25 +0300

    Remove comments from qutebrowser conf
    The information about the configuration can readily be found from the help
    docs. No need to duplicate it here.

--- .qutebrowser/config.py
+++ .qutebrowser/config.py
@@ -1,24 +1,4 @@
-## The position of the tab bar.
-## Type: Position
-## Valid values:
-##   - top
-##   - bottom
-##   - left
-##   - right
 c.tabs.position = 'left'
-
-## The width of the tab bar if it's vertical, in px or as percentage of
-## the window.
-## Type: PercOrInt
 c.tabs.width.bar = '8%'
 
-## Definitions of search engines which can be used via the address bar.
-## Maps a searchengine name (such as `DEFAULT`, or `ddg`) to a URL with a
-## `{}` placeholder. The placeholder will be replaced by the search term,
-## use `{{` and `}}` for literal `{`/`}` signs. The searchengine named
-## `DEFAULT` is used when `url.auto_search` is turned on and something
-## else than a URL was entered to be opened. Other search engines can be
-## used by prepending the search engine name to the search term, e.g.
-## `:open google qutebrowser`.
-## Type: Dict
 c.url.searchengines = {'DEFAULT': 'https://google.com/search?q={}'}
