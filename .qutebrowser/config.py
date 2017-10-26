32f77d1e2eb870335ef256000bfd9ca9445a5f0c
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-10-26 15:34:44 +0300

    Add youtube search engine

--- .qutebrowser/config.py
+++ .qutebrowser/config.py
@@ -1,7 +1,10 @@
 c.tabs.position = 'left'
 c.tabs.width.bar = '8%'
 
-c.url.searchengines = {'DEFAULT': 'https://google.com/search?q={}'}
+c.url.searchengines = {
+    'DEFAULT': 'https://google.com/search?q={}',
+    'youtube': 'https://www.youtube.com/results?search_query={}'
+}
 
 c.completion.shrink = True
 
